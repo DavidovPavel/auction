18a10eed45b1139f9d34663826d86c7952e76c97
Pavel <davidov.pavel@gmail.com>
2018-06-07 18:59:26 +0300

    sharede module, search component

--- auction/src/app/app.module.ts
+++ auction/src/app/app.module.ts
@@ -1,28 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { registerLocaleData } from '@angular/common';
 import localeRu from '@angular/common/locales/ru';
 registerLocaleData(localeRu, 'ru');
 
 import { RoutingModule } from './routing.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CaruselComponent } from './components/carusel/carusel.component';
 import { AclDirective } from './components/header/acl.directive';
-
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CaruselComponent,
-    AclDirective
+    AclDirective,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    RoutingModule
+    RoutingModule,
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [{ provide: LOCALE_ID, useValue: 'ru' }],
   bootstrap: [AppComponent]
